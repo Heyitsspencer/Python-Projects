4bf44b3130d2811af5656d98d97e57ef52a8d83b
Heyitsspencer <heyitsspencerdavis@gmail.com>
2022-10-25 14:22:46 -0500

    Create Phonebook Application

---
+++ Misc/PhonebookApplication.py
@@ -0,0 +1,24 @@
+# Python 3.9.7
+# Author: Spencer Davis
+# Purpose: Phonebook Application
+
+#First name label
+self.lbl_fname = tk.Label(self.master,text='First Name: ')
+self.lbl_fname.grid(row=0,column=0,padx=(27,0),pady=(10,0),sticky=N+W)
+
+#Last name label
+self.lbl_lname = tk.Label(self.master,text='Last Name: ')
+self.lbl_lname.grid(row=2,column=0,padx=(27,0),pady=(10,0),sticky=N+W)
+
+#Phone number label
+self.lbl_phone = tk.Label(self.master,text='Phone Number: ')
+self.lbl_phonee.grid(row=4,column=0,padx=(27,0),pady=(10,0),sticky=N+W)
+
+#Email address label
+self.lbl_email = tk.Label(self.master,text='Email Address: ')
+self.lbl_email.grid(row=6,column=0,padx=(27,0),pady=(10,0),sticky=N+W)
+
+#Information label
+self.lbl_info = tk.Label(self.master,text='Information: ')
+self.lbl_info.grid(row=0,column=2,padx=(27,0),pady=(10,0),sticky=N+W)
+    
--- Phonebook/phonebook_main.py
+++ Phonebook/phonebook_main.py
@@ -24,20 +24,25 @@ class ParentWindow(Frame):
         self.master = master
         self.master.minsize(500,300) #(Height, width)
         self.master.maxsize(500,300)
-        #This centerWindow method will center our app on the users screen
+        
+        # This centerWindow method will center our app on the users screen
         phonebook_func.center_window(self,500,300)
         self.master.title("The Tkinter Phonebook Demo")
         self.master.configure(bg="#F0F0F0")
-        #This protocol method is a tkinter built in method to catch if
-        #the user clicks the upper corner, 'X' on windows OS
+        
+        # This protocol method is a tkinter built in method to catch if
+        # the user clicks the upper corner, 'X' on windows OS
         self.master.protocol("WM_DELETE_WINDOW",lambda: phonebook_func.ask_quit(self))
         arg = self.master
 
-        #load in the GUI widgets from a seperate module
-        #keeping your code compartmentalized and clutter free
+        # load in the GUI widgets from a seperate module
+        # keeping your code compartmentalized and clutter free
         phonebook_gui.load_gui(self)
 
-        if __name__ == "__main__":
-            root = tk.Tk()
-            App = ParentWindow(root)
-            root.mainloop()
+
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    App = ParentWindow(root)
+    root.mainloop()
